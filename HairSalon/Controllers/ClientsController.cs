8d7c76be5e97137376178f476d9be4c8b940cdef
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-20 14:32:28 -0700

    add Create route and View for Clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -21,6 +21,7 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
       return View(model);
     }
+
     [HttpPost]
     public ActionResult Create(Client client)
     {
@@ -33,6 +34,11 @@ namespace HairSalon.Controllers
     {
       ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "StylistName");
       return View();
+    }
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      return View(thisClient);
     }
   }
 }
